b15735762bc25dc73cf01a8544f4ac78e54816e8
bung87 <crc32@qq.com>
2021-06-04 14:42:29 +0800

    code syntax highlight

--- example/themes/default/post.nim
+++ example/themes/default/post.nim
@@ -1,10 +1,14 @@
 import karax / [karaxdsl, vdom]
 import ./layout
-
-
+import packages/docutils/highlite
+import packages/docutils/rstgen
+import strutils
+include nmark #, nmark / [mdToAst,astToHtml, def]
+include nmark / insertMarker
+import htmlgen
 proc PurePost*(title = ""; id = ""; date = ""; cates: seq[string] = @[]; tags: seq[string] = @[];
     child: VNode = nil): VNode =
-  let post = buildHtml(tdiv()):
+  let post = buildHtml(tdiv(data-theme = "dark")):
     h4:
       text title
 
@@ -15,8 +19,76 @@ proc PurePost*(title = ""; id = ""; date = ""; cates: seq[string] = @[]; tags: s
     child
   PureLayout(post)
 
+proc dispA(target: OutputTarget; dest: var string;
+           xml, tex: string; args: varargs[string]) =
+  if target != outLatex: addf(dest, xml, args)
+  else: addf(dest, tex, args)
+
+proc renderCodeLang*(result: var string; lang: SourceLanguage; code: string;
+                     target: OutputTarget) =
+  var g: GeneralTokenizer
+  initGeneralTokenizer(g, code)
+  while true:
+    getNextToken(g, lang)
+    case g.kind
+    of gtEof: break
+    of gtNone, gtWhitespace:
+      add(result, substr(code, g.start, g.length + g.start - 1))
+    else:
+      dispA(target, result, "<span class=\"$2\">$1</span>", "\\span$2{$1}", [
+        esc(target, substr(code, g.start, g.length+g.start-1)),
+        tokenClassToStr[g.kind]])
+  deinitGeneralTokenizer(g)
+
+proc markdown2html(lines: string): string =
+  let seqAst = lines.mdToAst
+
+  var linkSeq: seq[Block]
+  for e in seqAst:
+    if e.kind == BlockKind.linkRef:
+      linkSeq.add(e)
+    elif e.kind == BlockKind.containerBlock:
+      for c in e.children:
+        if c.kind == BlockKind.linkRef:
+          linkSeq.add(c)
+    else:
+      continue
+  var isTight = false
+  for ast in seqAst:
+    case ast.kind
+      of fencedCode:
+        if ast.codeText == "":
+          if ast.codeAttr != "":
+            var code: string
+            renderCodeLang(code, getSourceLanguage(ast.codeAttr.insertInline(linkSeq)), ast.codeText,
+                OutputTarget.outHtml)
+            var t = pre(class = "highlight", code(code)) & "\p"
+            result.add t.replace("<code>", "<code class=\"language-" & ast.codeAttr.insertInline(linkSeq) & "\">")
+          else:
+            var code: string
+            renderCodeLang(code, getSourceLanguage(ast.codeAttr.insertInline(linkSeq)), ast.codeText,
+                OutputTarget.outHtml)
+            result.add pre(class = "highlight", code(code)) & "\p"
+
+        else:
+          if ast.codeAttr != "":
+            var code: string
+            renderCodeLang(code, getSourceLanguage(ast.codeAttr.insertInline(linkSeq)), ast.codeText,
+                OutputTarget.outHtml)
+            var t = pre(class = "highlight", code(code & "\p")) & "\p"
+            result.add t.replace("<code>", "<code class=\"language-" & ast.codeAttr.insertInline(linkSeq) & "\">")
+          else:
+            var code: string
+            renderCodeLang(code, getSourceLanguage(ast.codeAttr.insertInline(linkSeq)), ast.codeText,
+                OutputTarget.outHtml)
+            result.add pre(class = "highlight", code(code & "\p")) & "\p"
+      else:
+        result.add(ast.astToHtml(isTight, linkseq))
+
+  return result
+
 when isMainModule:
-  import nmark, nmark / [mdToAst, def]
+
   import os
   import yaml, json
   import regex
@@ -57,5 +129,5 @@ when isMainModule:
   var tags = newSeq[string]()
   for e in meta{"tags"}.getElems:
     tags.add e.getStr("")
-  let htmlContent = verbatim(restContent.markdown)
+  let htmlContent = verbatim(markdown2html(restContent))
   setRenderer proc(): VNode = PurePost(title, id, date, cates, tags, child = htmlContent)
