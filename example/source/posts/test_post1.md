89c05a36b69c27191881001ead59b83c05487ecf
bung87 <crc32@qq.com>
2021-06-04 13:08:36 +0800

    o

--- example/source/posts/test_post1.md
+++ example/source/posts/test_post1.md
@@ -6,3 +6,22 @@ categories:
 date: 2017-11-05 17:59:58
 tags:
 ---
+
+``` nim
+
+import strformat
+type
+  Person = object
+    name*: string # Field is exported using `*`.
+    age: Natural  # Natural type ensures the age is positive.
+
+var people = [
+  Person(name: "John", age: 45),
+  Person(name: "Kate", age: 30)
+]
+
+for person in people:
+  # Type-safe string interpolation.
+  echo(fmt"{person.name} is {person.age} years old")
+
+```
--- example/themes/default/post.nim
+++ example/themes/default/post.nim
@@ -2,25 +2,60 @@ import karax / [karaxdsl, vdom]
 import ./layout
 
 
-proc PurePost*(title: string = ""; n: VNode = nil): VNode =
-  let post = buildHtml(tdiv())
+proc PurePost*(title = ""; id = ""; date = ""; cates: seq[string] = @[]; tags: seq[string] = @[];
+    child: VNode = nil): VNode =
+  let post = buildHtml(tdiv()):
+    h4:
+      text title
+
+    tdiv(class = "post-meta"):
+      span:
+        span(class = "far fa-calendar-alt", aria-hidden = "true")
+        text date
+    child
   PureLayout(post)
 
 when isMainModule:
-  import nmark / [mdToAst, def]
+  import nmark, nmark / [mdToAst, def]
   import os
   import yaml, json
+  import regex
   const sourceDir = currentSourcePath.parentDir.parentDir.parentDir / "source"
   const postDir = sourceDir / "posts"
   const filePath = postDir / "test_post1.md"
   const content = staticRead(postDir / "test_post1.md")
   let seqAst = content.mdToAst
 
+  var prob = 0
+  var pra: string
+  # for i,node in seqAst:
+  #   if prob == 2 and node.kind == BlockKind.containerBlock:
+  #     echo repr node.children
+  #   if node.kind == BlockKind.leafBlock:
+  #     if (i == 0 or prob > 1) and node.leafType == thematicBreak :
+  #       inc prob
+  #     elif i == 1 and node.leafType == paragraph:
+  #       pra = node.raw
+  #       inc prob
+  var m: RegexMatch
+  let ret = content.find(re"(?ms:-{3,}(.*)-{3,})", m)
+  let restContent = content[m.boundaries.b + 1 .. ^1]
+  pra = m.group(0, content)[0]
+  var meta: JsonNode = newJObject()
 
-  for node in seqAst:
-    if node.kind == BlockKind.leafBlock:
-      if node.leafType == paragraph:
-        var parser = initYamlParser(true)
-        var ys = parser.parse(node.raw)
-        echo constructJson(ys)[0]
-  setRenderer proc(): VNode = PurePost()
+  var parser = initYamlParser(true)
+  var ys = parser.parse(pra)
+  meta = constructJson(ys)[0]
+  echo meta
+
+  let title = meta{"title"}.getStr("")
+  let id = meta{"id"}.getStr("")
+  var cates = newSeq[string]()
+  for e in meta{"categories"}.getElems:
+    cates.add e.getStr("")
+  let date = meta{"date"}.getStr("")
+  var tags = newSeq[string]()
+  for e in meta{"tags"}.getElems:
+    tags.add e.getStr("")
+  let htmlContent = verbatim(restContent.markdown)
+  setRenderer proc(): VNode = PurePost(title, id, date, cates, tags, child = htmlContent)
